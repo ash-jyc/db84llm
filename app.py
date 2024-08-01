0200448885c1923fb1d01774ed02f374338fd278
Ashley <ajychen26@gmail.com>
2024-08-01 16:02:37 -0700

    fix port

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def caselist_api():
     return jsonify(caselist_data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
