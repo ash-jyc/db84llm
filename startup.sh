da986c46e1dce7e7cd469b636e7e0869731a38ff
Ashley <ajychen26@gmail.com>
2024-08-01 17:48:17 -0700

    port spec

--- startup.sh
+++ startup.sh
@@ -21,5 +21,8 @@ ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # Install unixodbc-dev for pyodbc
 apt-get install -y unixodbc-dev
 
-# Start the Gunicorn server
-gunicorn -w 3 app:app
+# Get the port from the environment variable or default to 8000
+port=${PORT:-8000}
+
+# Start Gunicorn server
+gunicorn -w 3 -b 0.0.0.0:$port app:app
